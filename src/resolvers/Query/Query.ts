3c54b74356742b971b7d82082987ab5489abd5fa
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-03-08 11:51:44 +0600

    complete

--- src/resolvers/Query/Query.ts
+++ src/resolvers/Query/Query.ts
@@ -20,6 +20,7 @@ export const Query = {
   },
 
   posts: async (parent: any, args: any, { prisma }: any) => {
+    console.log("post");
     return await prisma.post.findMany({
       where: {
         published: true,
